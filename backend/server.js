9489209eff12c9dbbae694916b5a1850c11a6840
bb <bishopbansal2005@gmail.com>
2025-12-04 19:24:35 +0530

    back to original

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,39 @@
 import express from 'express';
 import dotenv from "dotenv";
 import path from 'path';
-import { fileURLToPath } from "url";
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 import productRoutes from "./routes/product.route.js";
-
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 5030;
+const app=express();
+const Port=process.env.PORT||5030;
 
-// ES Module dirname fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname=path.resolve();
+
 
 app.use(express.json());
 
-// API routes
-app.use("/api/products", productRoutes);
+app.use("/api/products",productRoutes);
 
-// --- Production Build Serve ---
-if (process.env.NODE_ENV === "production") {
-    const frontendPath = path.join(__dirname, "../frontend/dist");
-
-    app.use(express.static(frontendPath));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(frontendPath, "index.html"));
-    });
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
 }
 
-app.listen(PORT, () => {
-    connectDB();
-    console.log(`🚀 Server running on port ${PORT}`);
-});
+
+
+
+
+
+app.listen(Port,()=>{
+    connectDB(),
+    console.log("server started at http://localhost:5000  Port is", Port);
+})
+
+
+
+
+
